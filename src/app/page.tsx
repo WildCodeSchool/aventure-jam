d639a56f604148c5192d42a86d01ed7724bd9c9c
Jerome Marbach <jeromemarbach@outlook.fr>
2025-06-19 12:20:40 +0200

    i creat button link for About pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import styles from "./page.module.css";
+import About from "./a-propos/pages";
 
 const Home = () => {
   return (
@@ -6,6 +7,11 @@ const Home = () => {
       <div className={styles.mainTitle}>
         <h1>Interactive History</h1>
         <button className={styles.linkHistory}>HISTOIRE</button>
+      </div>
+      <div className={styles.aboutSection}>
+        <a href="src/app/a-propos/pages.tsx">
+          <img className={styles.linkAbout} src="images/9195785.png" alt="" />
+        </a>
       </div>
     </>
   );
