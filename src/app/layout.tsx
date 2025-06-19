d86b4f60a9dd57987cfdc715c219a3e17d48319c
Adrien Meert <adrien.meert92@gmail.com>
2025-06-19 16:23:22 +0200

    Modif layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
-import Header from "@/components/Header";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
-        <Header />
         <main>{children}</main>
       </body>
     </html>
