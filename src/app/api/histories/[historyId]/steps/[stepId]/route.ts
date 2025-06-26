bf5e07bec4a3c1b34684b6a3127e490cba17c0b7
Jerome Marbach <jeromemarbach@outlook.fr>
2025-06-26 22:53:55 +0200

    i create branch etape and files steps

--- src/app/api/histories/[historyId]/steps/[stepId]/route.ts
+++ src/app/api/histories/[historyId]/steps/[stepId]/route.ts
@@ -2,16 +2,18 @@ import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET(
-  request: Request,
-  { params }: { params: Promise<{ historyId: number; stepId: number }> }
+  _request: Request,
+  { params }: { params: { historyId: number; stepId: number } }
 ) {
-  const { historyId, stepId } = await params;
+  const { stepId } = params;
 
   console.log("hello");
 
   try {
     const [rows] = await db.query(
-      "SELECT id, texte, hystory_id, pnj, background FROM etape WHERE id = ?"
+      "SELECT id, texte, history_id, pnj, background FROM etape WHERE id = ?"[
+        stepId
+      ]
     );
     return NextResponse.json(rows);
   } catch (error) {
--- src/app/etapes/pages.tsx
+++ src/app/etapes/pages.tsx
@@ -3,7 +3,6 @@ import { apiRoutes } from "@/data/ROUTES";
 const Step = async () => {
   const apiResult = await fetch(apiRoutes.STEP(1, 1));
   const steps = await apiResult.json();
-  console.log(steps);
 };
 
 export default Step;
