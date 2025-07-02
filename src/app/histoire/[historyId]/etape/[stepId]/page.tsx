6492f48b1a8d2ac50c229e008ba25fcd30e7ab03
Jerome Marbach <jeromemarbach@outlook.fr>
2025-07-02 12:24:08 +0200

    I create two links in the step that allow me to  navigate between the different steps.

--- src/app/histoire/[historyId]/etape/[stepId]/page.tsx
+++ src/app/histoire/[historyId]/etape/[stepId]/page.tsx
@@ -1,5 +1,6 @@
 import { apiRoutes } from "@/data/ROUTES";
 import styles from "./etape.module.css";
+import Link from "next/link";
 
 type Props = {
   params: {
@@ -10,15 +11,36 @@ type Props = {
 
 const Step = async ({ params }: Props) => {
   const { historyId, stepId } = await params;
+  const toNextStep = Number(stepId);
+  const toPreviewStep = Number(stepId);
   const apiResult = await fetch(apiRoutes.STEP(historyId, stepId));
   const step = await apiResult.json();
 
   return (
-    <section className={styles.etapeBody}>
-      <div className={styles.mainEtapeTitle}>
-        <p>{step.texte}</p>
-      </div>
-    </section>
+    <>
+      <section className={styles.etapeBody}>
+        <div className={styles.accueilSection}>
+          <Link href="/">
+            <img
+              className={styles.linkAccueil}
+              src="/images/9713317.png"
+              alt=" aller vers accueil"
+            />
+          </Link>
+        </div>
+        <div className={styles.mainEtapeTitle}>
+          <p>{step.texte}</p>
+          <div className={styles.nextPage}>
+            <Link href={`/histoire/${historyId}/etape/${toNextStep + 1}`}>
+              Etape suivante
+            </Link>
+            <Link href={`/histoire/${historyId}/etape/${toNextStep - 1}`}>
+              Etape precedente
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
