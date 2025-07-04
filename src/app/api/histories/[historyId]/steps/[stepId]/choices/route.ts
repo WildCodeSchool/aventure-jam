31b7fcb88716a54e0e9136ee3f1c64ab87a419a7
Jerome Marbach <jeromemarbach@outlook.fr>
2025-07-04 11:23:27 +0200

    I create the choice route in order to capture the corresponding API.

---
+++ src/app/api/histories/[historyId]/steps/[stepId]/choices/route.ts
@@ -0,0 +1,28 @@
+import { db } from "@/lib/db";
+import { StepModel } from "@/model/StepModel";
+import { NextResponse } from "next/server";
+
+type Params = {
+  params: { stepId: string; choiceId: string };
+};
+
+export async function GET(_req: Request, { params }: Params) {
+  const { stepId } = await params;
+  try {
+    const result = await db.query(
+      "SELECT id, texte, etape_id FROM choice WHERE etape_id = ?",
+      [stepId]
+    );
+    const rows = result[0] as StepModel[];
+    if (rows.length === 0) {
+      return NextResponse.json({ error: "Etape non trouvée" }, { status: 404 });
+    }
+    return NextResponse.json(rows);
+  } catch (error) {
+    console.error("erreur MySql : ", error);
+    return NextResponse.json(
+      { error: " Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
