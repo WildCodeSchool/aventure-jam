f4ff6ee9ab1d27ebff521f779255078405a7e652
Jerome Marbach <jeromemarbach@outlook.fr>
2025-07-11 10:31:43 +0200

    i fix background in middleware page

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,25 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export default function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+const PAGE_PROTECTED_PATHS = [/^\/histoire\/\d+\/etape\/\d+$/];
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const userToken = request.cookies.get("user-token");
+
+  const isProtected = PAGE_PROTECTED_PATHS.some((path) =>
+    typeof path === "string" ? pathname === path : path.test(pathname)
+  );
+
+  if (isProtected && !userToken) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/about/:path*",
+  matcher: ["/histoire/:path*/etape/:path*/", "/histoire/:path*"],
 };
