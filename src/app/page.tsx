1824ec6ea6169f4205a91d3877123865b1806601
Jerome Marbach <jeromemarbach@outlook.fr>
2025-07-17 15:35:38 +0200

    i create component Modal fot force the user to connect to google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,39 @@
+"use client";
 import Link from "next/link";
 import styles from "./page.module.css";
 import Login from "@/ui/login";
 import { appRoutes } from "@/data/ROUTES";
+import { signIn, useSession } from "next-auth/react";
+import { useState } from "react";
+import Modal from "@/ui/Modal";
 
 const Home = () => {
+  const { data: session } = useSession();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleProtectedClick = () => {
+    setIsModalOpen(true);
+  };
+  console.log(isModalOpen);
   return (
     <>
       <section className={styles.homeBody}>
         <div className={styles.mainTitle}>
           <h1>HISTOIRE INTERACTIVE</h1>
+          {session ? (
+            <Link className={styles.linkHistory} href={appRoutes.HISTORY(1)}>
+              LANCER HISTOIRE
+            </Link>
+          ) : (
+            <button
+              type="button"
+              onClick={handleProtectedClick}
+              className={styles.linkHistory}
+            >
+              LANCER HISTOIRE
+            </button>
+          )}
 
-          <Link className={styles.linkHistory} href={appRoutes.HISTORY(1)}>
-            LANCER HISTOIRE
-          </Link>
           <div className={styles.googleButton}>
             <Login />
           </div>
@@ -27,6 +48,14 @@ const Home = () => {
           </Link>
         </div>
       </section>
+      {isModalOpen && (
+        <Modal
+          onClose={() => setIsModalOpen(false)}
+          onLogin={() => {
+            signIn("google");
+          }}
+        />
+      )}
     </>
   );
 };
