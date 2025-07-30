29cffaca7414c4b519140d1a0aab1eb26d1a54fc
Maxence Agullo <magullo35@gmail.com>
2025-07-30 10:38:58 +0200

    added pictures for objects

--- src/components/ButtonValidation.tsx
+++ src/components/ButtonValidation.tsx
@@ -53,10 +53,9 @@ const ButtonToValidate = ({ link, label, objectId, takeOrGive }: Props) => {
         if (email && historyId && objectId !== null) {
           if (takeOrGive && takeOrGive === 0) {
             addInventory(email, historyId, objectId);
-          } else if (takeOrGive && takeOrGive === 1) {
+          }
+          if (takeOrGive && takeOrGive === 1) {
             deleteInventories(email, historyId, [objectId])
-          } else {
-            return
           }
         }
       } catch (error) {
